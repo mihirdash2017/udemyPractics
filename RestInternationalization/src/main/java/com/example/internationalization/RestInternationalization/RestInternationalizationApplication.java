d28807da8be7ca18d2e34cfc6f9af3d547616790
user <user@user-PC>
2019-09-27 21:14:31 +0530

    project structure chekin

---
+++ RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/RestInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.internationalization.RestInternationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestInternationalizationApplication.class, args);
+	}
+
+}
