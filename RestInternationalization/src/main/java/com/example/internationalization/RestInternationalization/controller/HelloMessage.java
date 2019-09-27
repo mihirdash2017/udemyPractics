e0c9abf3c64d98586c824e45e40d345ca6108d60
user <user@user-PC>
2019-09-27 22:26:56 +0530

    commite :controller ready

--- RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/controller/HelloMessage.java
+++ RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/controller/HelloMessage.java
@@ -1,7 +1,9 @@
 package com.example.internationalization.RestInternationalization.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloMessage {
 	
 	@GetMapping(value="/msg")
