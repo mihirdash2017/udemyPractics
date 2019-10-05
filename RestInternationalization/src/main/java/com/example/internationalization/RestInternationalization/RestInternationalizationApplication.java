d0ba9c7e1240bdf49928a944cf31a3230cfae510
user <user@user-PC>
2019-10-05 13:12:46 +0530

    commit :done with internationalization

--- RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/RestInternationalizationApplication.java
+++ RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/RestInternationalizationApplication.java
@@ -1,13 +1,36 @@
 package com.example.internationalization.RestInternationalization;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+
+
 
 @SpringBootApplication
 public class RestInternationalizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestInternationalizationApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver doResolveLocale(){
+		SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+		
+	}
+	
+	@Bean(name="messageSource")
+	ResourceBundleMessageSource bundleMessageSource(){
+		ResourceBundleMessageSource messagesource=new ResourceBundleMessageSource();
+		messagesource.setBasename("message");
+		return messagesource;
 	}
 
 }
--- RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/controller/HelloMessage.java
+++ RestInternationalization/src/main/java/com/example/internationalization/RestInternationalization/controller/HelloMessage.java
@@ -1,14 +1,22 @@
 package com.example.internationalization.RestInternationalization.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloMessage {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(value="/msg")
-	public String getMessage(){
-		return "good Morning";
+	public String getMessage(@RequestHeader(name="Accept-Language",required=false) Locale locale){
+		return messageSource.getMessage("good.morning.message",null, locale);
 	}
 
 }
