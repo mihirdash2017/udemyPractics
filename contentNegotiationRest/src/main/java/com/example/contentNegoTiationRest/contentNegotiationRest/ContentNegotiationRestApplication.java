42c52b76eb17729624487b29f2eb8fc91ccac95f
user <user@user-PC>
2019-10-05 13:20:47 +0530

    commit :project structre ready for contentNegotiationRest

---
+++ contentNegotiationRest/src/main/java/com/example/contentNegoTiationRest/contentNegotiationRest/ContentNegotiationRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.contentNegoTiationRest.contentNegotiationRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentNegotiationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentNegotiationRestApplication.class, args);
+	}
+
+}
---
+++ contentNegotiationRest/src/test/java/com/example/contentNegoTiationRest/contentNegotiationRest/ContentNegotiationRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.contentNegoTiationRest.contentNegotiationRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContentNegotiationRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
