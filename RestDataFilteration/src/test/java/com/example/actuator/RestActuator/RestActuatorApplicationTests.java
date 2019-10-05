608a00ce6591a88e03a448d13dd54198cc2c969f
user <user@user-PC>
2019-10-05 23:41:08 +0530

    commit :project structure is ready for rest data filterartion

---
+++ RestDataFilteration/src/test/java/com/example/actuator/RestActuator/RestActuatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.actuator.RestActuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestActuatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
