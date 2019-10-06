9d88a2bab138ebb1bfb04f9affed7cffadc60a7b
user <user@user-PC>
2019-10-06 16:20:56 +0530

    commit:project structure is ready for rest versioning

---
+++ RestVersion/src/main/java/com/example/restversion/RestVersion/RestVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.restversion.RestVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVersionApplication.class, args);
+	}
+
+}
